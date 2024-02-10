15038e42c0769d6ed3cedb3ee35500cbe5c6ec8e
Ted Lehr <ted.lehr@txstate.edu>
2024-02-10 13:21:49 -0600

    1st text-to-audio using  static route & deprecated stream_to_file

--- intro_to_flask/speak_python/speak_route.py
+++ intro_to_flask/speak_python/speak_route.py
@@ -6,33 +6,35 @@ from markupsafe import escape #protects projects against injection attacks
 from intro_to_flask import app
 import sys 
 sys.dont_write_bytecode = True
-from flask import render_template, request, Flask, Blueprint
+from flask import render_template, request, Flask, Blueprint, Response
 from .speak_form import SpeakmeForm
 
 speak_blueprint = Blueprint('speakme', __name__)
 
 @speak_blueprint.route('/speakme',methods=['GET', 'POST'])
 @app.route('/speakme',methods=['GET', 'POST'])
-def drawme():
+def speakme():
   form = SpeakmeForm(request.form)
   
   if request.method == 'POST':
       if form.validate() == False:
-        return render_template('drawme.html', form=form)
+        return render_template('speakme.html', form=form)
       else:
         # The following response code adapted from example on: 
         # https://platform.openai.com/docs/api-reference/images
         client = OpenAI()
+        
+        speech_file_name = "createspeech.mp3"
+        speech_file_path = "intro_to_flask/static/" + speech_file_name
 
-        response = client.images.generate(
-          model="dall-e-3",
-          prompt=form.prompt.data,
-          quality="standard",
-          size="1024x1024",
-          n=1,
+        response = client.audio.speech.create(
+          model="tts-1",
+          voice="alloy",
+          input=form.prompt.data,
         )
-        display_image_url = response.data[0].url
-        return render_template('speakme.html', speak_me_prompt=form.prompt.data,speak_me_response=display_image_url,success=True)
+        response.stream_to_file(speech_file_path)
+
+        return render_template('speakme.html', speak_me_prompt=form.prompt.data,speak_me_response=speech_file_name,success=True)
       
   elif request.method == 'GET':
-      return render_template('drawme.html', form=form)
+      return render_template('speakme.html', form=form)
