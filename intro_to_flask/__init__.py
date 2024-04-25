ad9b7451a63bea6073db802a283a9d682e5f45d9
Ted Lehr <ted.lehr@txstate.edu>
2024-04-25 09:30:18 -0500

    More corrections of conflicts not removed by merge

--- intro_to_flask/__init__.py
+++ intro_to_flask/__init__.py
@@ -10,8 +10,3 @@ app.config['SECRET_KEY']='LongAndRandomSecretKey'
 
 #mail.init_app(app)
 from .routes import mail
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://your-username:your-password@localhost/development'
-from models import db
-db.init_app(app)
-mail.init_app(app)
