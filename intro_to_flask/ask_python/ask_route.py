260831fa1f22fa942a6e7b5eaa23a316f70b2d84
Alan Solis <ajs289@txstate.edu>
2024-05-01 18:27:27 -0500

    implemented audio output of ChatGPT response on "Ask Me" page

--- intro_to_flask/ask_python/ask_route.py
+++ intro_to_flask/ask_python/ask_route.py
@@ -7,7 +7,7 @@ from markupsafe import escape #protects projects against injection attacks
 from intro_to_flask import app
 import sys 
 sys.dont_write_bytecode = True
-from flask import render_template, request, Flask, Blueprint
+from flask import render_template, request, Flask, Blueprint,send_from_directory, url_for
 from .ask_form import AskmeForm
 from ..utilities.form_parsing  import parse_SelectField_choices
 
@@ -49,8 +49,17 @@ def askme():
         speech_file_name = "askme_response.mp3"
         speech_file_path = os.path.join("intro_to_flask/temp/",speech_file_name)
 
-        display_text = response.choices[0].message.content
-        return render_template('askme.html', ask_me_prompt=form.userprompt.data,ask_me_response=display_text,success=True)
+        response = client.audio.speech.create(
+          model="tts-1",
+          voice="alloy",
+          input=response.choices[0].message.content,
+        )
+        response.stream_to_file(speech_file_path)
+        speech_url = url_for('temp.gettempfile', filename=speech_file_name)
+
+        print(speech_url) 
+
+        return render_template('askme.html', ask_me_prompt=form.userprompt.data,ask_me_response=display_text,ask_me_audio=speech_url,success=True)
       
   elif request.method == 'GET':
       return render_template('askme.html', form=form)
