0435480459a6775eebe1daa338429443fee89bc9
Alan Solis <ajs289@txstate.edu>
2024-05-01 19:08:01 -0500

    created new dropdown menus for "Draw Me"

--- intro_to_flask/draw_python/draw_form.py
+++ intro_to_flask/draw_python/draw_form.py
@@ -4,8 +4,24 @@ sys.dont_write_bytecode = True
 # pip install flask-wtf
 # pip install email_validator
 from flask_wtf import Form
-from wtforms import TextAreaField, SubmitField, validators, ValidationError
+from wtforms import TextAreaField, SelectField, SubmitField, validators, ValidationError
+from wtforms.validators import DataRequired
 
 class DrawmeForm(Form):
     prompt = TextAreaField("What would you like to draw?",  [validators.InputRequired("Please enter a prompt.")])
+
+    image_quality = SelectField("Image Quality", 
+                               choices=[('standard'), 
+                                        ('hd')
+                                        ],
+                               validators=[DataRequired()],
+                               coerce=str)
+    image_size = SelectField("Image Size", 
+                               choices=[('1024x1024'),
+                                        ('1792x1024'),
+                                        ('1024x1792')
+                                        ],
+                               validators=[DataRequired()],
+                               coerce=str)
+
     submit = SubmitField("Send") 
